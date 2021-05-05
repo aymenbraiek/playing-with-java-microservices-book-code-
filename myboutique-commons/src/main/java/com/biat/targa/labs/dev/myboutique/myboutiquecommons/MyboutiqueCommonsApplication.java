d908215abd048b6bfd778b17c0b0c2e321d1427a
aymenbraiek <aymen.braiek@esprit.tn>
2021-05-05 14:47:42 +0100

    adding myboutique-commons project

---
+++ myboutique-commons/src/main/java/com/biat/targa/labs/dev/myboutique/myboutiquecommons/MyboutiqueCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.biat.targa.labs.dev.myboutique.myboutiquecommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboutiqueCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboutiqueCommonsApplication.class, args);
+	}
+
+}
---
+++ myboutique-commons/src/main/java/com/biat/targa/labs/dev/myboutique/myboutiquecommons/commons/utils/Web.java
@@ -0,0 +1,9 @@
+package com.biat.targa.labs.dev.myboutique.myboutiquecommons.commons.utils;
+
+/**
+ *
+ * @author n.lamouchi
+ */
+public class Web {
+    public static final String API = "/api";
+}
